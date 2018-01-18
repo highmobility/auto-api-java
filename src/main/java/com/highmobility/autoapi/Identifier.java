64d0297030aecfe4bb1fa99749f6a525119db448
Tõnis Tiganik <ttiganik@gmail.com>
2018-01-18 18:28:35 +0700

    add license headers

--- src/main/java/com/highmobility/autoapi/Identifier.java
+++ src/main/java/com/highmobility/autoapi/Identifier.java
@@ -1,9 +1,30 @@
+/*
+ * HMKit Auto API - Auto API Parser for Java
+ * Copyright (C) 2018 High-Mobility <licensing@high-mobility.com>
+ *
+ * This file is part of HMKit Auto API.
+ *
+ * HMKit Auto API is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * HMKit Auto API is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HMKit Auto API.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.highmobility.autoapi;
 
 import com.highmobility.utils.Bytes;
 
-// used for generating type bytes, internally only.
-// will not use for public because unknown commands need support as well
+/**
+ * Used for identifying known auto api categories
+ */
 public enum Identifier {
     FAILURE(new byte[] { 0x00, (byte)0x02 }),
     CAPABILITIES(new byte[] { 0x00, (byte)0x10 }),
