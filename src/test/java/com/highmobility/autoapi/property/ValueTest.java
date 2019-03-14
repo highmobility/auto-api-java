69d97cc5cdd53915586f00ceab99e5489a5ad4dc
Tõnis Tiganik <ttiganik@high-mobility.com>
2019-03-14 11:58:33 +0700

    add universal timestamp test

--- src/test/java/com/highmobility/autoapi/property/ValueTest.java
+++ src/test/java/com/highmobility/autoapi/property/ValueTest.java
@@ -6,8 +6,7 @@ import static junit.framework.TestCase.assertTrue;
 
 public class ValueTest {
     @Test public void testUnsignedIntOverflow() {
-        // TODO: 2019-03-07 drivernumber is unsigned. test if there is overflow eg > 128
-
+        // test if there is overflow eg > 128
         int value = 130;
         byte byteValue = Property.intToBytes(value, 1)[0];
         assertTrue(byteValue == (byte)0x82);
