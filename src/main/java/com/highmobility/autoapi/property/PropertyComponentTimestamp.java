ce5be1731678ff035071ca62023a70a2368e9499
Tõnis Tiganik <ttiganik@high-mobility.com>
2019-05-24 11:03:34 +0800

    add exception description

--- src/main/java/com/highmobility/autoapi/property/PropertyComponentTimestamp.java
+++ src/main/java/com/highmobility/autoapi/property/PropertyComponentTimestamp.java
@@ -37,7 +37,7 @@ public class PropertyComponentTimestamp extends PropertyComponent {
 
     public PropertyComponentTimestamp(Bytes bytes) {
         super(bytes);
-        if (getLength() < 11) throw new IllegalArgumentException();
+        if (getLength() < 11) throw new IllegalArgumentException("Invalid timestamp length");
         timestamp = Property.getCalendar(this, 3);
 
     }
