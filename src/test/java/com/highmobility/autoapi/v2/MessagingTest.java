b4bbcd87aef9f9e301350492a05e931fa308841b
Tõnis Tiganik <ttiganik@high-mobility.com>
2019-09-26 10:51:06 +0700

    fix more tests

---
+++ src/test/java/com/highmobility/autoapi/v2/MessagingTest.java
@@ -0,0 +1,55 @@
+/*
+package com.highmobility.autoapi.v2;
+
+import com.highmobility.autoapi.v2.property.Property;
+import com.highmobility.value.Bytes;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+*/
+/**
+ * Created by ttiganik on 15/09/16.
+ *//*
+public class MessagingTest extends BaseTest {
+    Bytes bytes = new Bytes(
+            "003701" +
+                    "01001101000e2b31203535352d3535352d353535" +
+                    "02001001000d48656c6c6f20796f7520746f6f");
+
+    @Test public void send() {
+        Command command = CommandResolver.resolve(bytes);
+        testState((SendMessage) command);
+    }
+
+    private void testState(SendMessage state) {
+        assertTrue(state.getRecipientHandle().getValue().equals("+1 555-555-555"));
+        assertTrue(state.getMessage().getValue().equals("Hello you too"));
+        assertTrue(bytesTheSame(state, bytes));
+    }
+
+    @Test public void received() {
+        Bytes waitingForBytes = new Bytes("003700" +
+                "01001101000e2b31203535352d3535352d353535" +
+                "02000801000548656c6c6f");
+
+        byte[] commandBytes = null;
+        commandBytes = new MessageReceived("+1 555-555-555", "Hello").getByteArray();
+
+        assertTrue(waitingForBytes.equals(commandBytes));
+
+        MessageReceived command = (MessageReceived) CommandResolver.resolve(waitingForBytes);
+        assertTrue(command.getSenderHandle().getValue().equals("+1 555-555-555"));
+        assertTrue(command.getMessage().getValue().equals("Hello"));
+    }
+
+    @Test public void build() {
+        SendMessage.Builder builder = new SendMessage.Builder();
+        builder.setRecipientHandle(new Property("+1 555-555-555"));
+        builder.setMessage(new Property("Hello you too"));
+        testState(builder.build());
+    }
+}*/
+
+// TODO: 26/09/2019  SendMessage is missing
